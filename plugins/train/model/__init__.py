80f7ce0cfbfdc11ed4eb5ec320b360ab187befff
aopsr <113384854+aopsr@users.noreply.github.com>
2023-11-11 02:00:52 -0500

    Update __init__.py

---
+++ plugins/train/model/__init__.py
@@ -0,0 +1,7 @@
+from .efficientnetv2_lite import EfficientNetV2B0
+from .efficientnetv2_lite import EfficientNetV2B1
+from .efficientnetv2_lite import EfficientNetV2B2
+from .efficientnetv2_lite import EfficientNetV2B3
+from .efficientnetv2_lite import EfficientNetV2L
+from .efficientnetv2_lite import EfficientNetV2M
+from .efficientnetv2_lite import EfficientNetV2S
